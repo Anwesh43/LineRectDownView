3c0dc351478e39a71e80c599a88a634d8dd827fa
Anwesh43 <anweshthecool0@gmail.com>
2020-11-12 02:53:17 +0530

    adding project setup

--- linerectdownview/src/main/java/com/example/linerectdownview/LineRectDownView.kt
+++ linerectdownview/src/main/java/com/example/linerectdownview/LineRectDownView.kt
@@ -1,2 +1,25 @@
 package com.example.linerectdownview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#3F51B5",
+    "#2196F3",
+    "#FF9800",
+    "#4CAF50"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
